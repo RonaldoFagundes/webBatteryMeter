0a71a31548a89ddd42e79a6a8aa6605d2cab8236
RonaldoFagundes <ronaldofagundes@gmail.com>
2025-07-05 00:32:16 -0300

    fix/0407

--- js/script.js
+++ js/script.js
@@ -9,7 +9,6 @@ const endpoint = "http://localhost:3322";
 var fkStation = '';
 
 
-
 const btn_list = document.getElementById('list');
 const btn_report = document.getElementById('report');
 
@@ -23,25 +22,21 @@ const btn_pdf_report = document.getElementById('pdf-report');
 
 
 const container_list = document.getElementById('container-list');
-
 const container_report = document.getElementById('container-report');
 
 
 const p_atention = document.getElementById('p-atention');
-
 const p_critical = document.getElementById('p-critical');
 
 
 
 const title_list = document.getElementById('title-list');
-
 const title_report = document.getElementById('title-report');
 
 
 
 
 const table_list = document.getElementById('table-list');
-
 const table_report = document.getElementById('table-report');
 
 
@@ -59,12 +54,25 @@ const dataBattery = [];
 btn_list.addEventListener('click', function () {
 	container_list.style.display = "block";
 	listBattery();
+
+	btn_graph.disabled = false;
+
+	input_search_battery.disabled = true;
+	input_search_battery.style.opacity = "0.33"
+
+	input_search_analysis.disabled = true;
+	input_search_analysis.style.opacity = "0.33"
+
 });
 
 
 
+
+
 btn_graph.addEventListener('click', function () {
-	loadGraph();
+	//loadGraph();
+	lineGraph();
+	btn_graph.disabled = true;
 });
 
 
@@ -76,15 +84,13 @@ btn_report.addEventListener('click', function () {
 
 
 
-
-
 select_battery.onchange = function () {
 
 	/*
 	Array.from(document.getElementById('table-report').tBodies).forEach((x, i) => {
 		if (i !== -1) x.remove();
 	});
-    */
+	*/
 
 	fkStation = this.value.slice(-2);
 	listReportByFk();
@@ -94,161 +100,156 @@ select_battery.onchange = function () {
 
 
 
+const radio_ref_id = document.getElementById("radio-ref-id");
+radio_ref_id.addEventListener("click", () => {
+
+	input_search_battery.disabled = false;
+	input_search_battery.style.opacity = "1"
+})
+
+
+
+
+const radio_ref_date = document.getElementById("radio-ref-date");
+radio_ref_date.addEventListener("click", () => {
+
+	input_search_analysis.disabled = false;
+	input_search_analysis.style.opacity = "1"
+})
+
+
+
+
+
 const input_search_battery = document.getElementById("input-search-battery");
-const radio_bigger = document.getElementById("radio-bigger");
+const radio_worst = document.getElementById("radio-worst");
 const radio_all_battery = document.getElementById("radio-all-battery");
 const tbody_battery = document.getElementById("tbody-battery");
 
-
-input_search_battery.addEventListener("keyup",(e)=>{
+input_search_battery.addEventListener("keyup", (e) => {
 
 	//const search = list_battery.filter(i=> i.obs.toLowerCase().includes(e.target.value.toLowerCase()));
 
-	const search = list_battery.filter(i=> i.id.toString().toLowerCase().includes(e.target.value.toLowerCase()));
-	
+	const search = list_battery.filter(i => i.id.toString().toLowerCase().includes(e.target.value.toLowerCase()));
+
 	displayListBattery(search);
 
-	//console.log(search);
-	
+	//console.log(search);	
 })
 
 
-radio_bigger.addEventListener("click",()=>{	
+
+radio_worst.addEventListener("click", () => {
 	const search = list_battery.filter((i) =>
-      i.condutancia > 4000
-    );
-    displayListBattery(search);	
+		i.condutancia < 3989
+	);
+	displayListBattery(search);
 })
 
-radio_all_battery.addEventListener("click",()=>{	
+
+radio_all_battery.addEventListener("click", () => {
 	displayListBattery(list_battery);
 })
 
 
-/*
-const onLoadListBattery = () =>{
 
-   title_list.innerHTML ="List Battery";
 
-   tbody_battery.innerHTML ="";
 
-   list_battery.forEach(e => {
 
-        label.push(e.id);
-		dataBattery.push((e.condutancia / 5000 * 100).toFixed(0));
+
+const displayListBattery = list_battery => {
+
+	title_list.innerHTML = "List Battery";
+
+	tbody_battery.innerHTML = "";
+
+
+	list_battery.forEach(e => {
 
 		let status = "";
-		let sign = "";       
+		let sign = "";
 
-		if (e.condutancia <= 4000) {           
+		let ref = 3989;
+		let dif = e.condutancia - ref;
+		let desvio = (dif / ref * 100).toFixed(0);
+
+
+        // console.log(e.condutancia + " " + percent + "%"); 
+
+        if (desvio >= -21 && desvio <= -20) {
+			//console.log(" Atenção " + percent);
+			status = "yellow";
+			p_atention.innerHTML = `${desvio}%  id ${e.id}`;			
+			p_atention.style.color = "yellow";			
+		
+		} else if (desvio >= -45 && desvio <= -44) {
+			//console.log(" Critico " + percent);
 			status = "red";
-			p_critical.innerHTML = `Critico ${e.condutancia} id ${e.id}`; 
-			p_critical.style.backgroundColor = "black"; 
-			p_critical.style.color = "red";
-		}else if(e.condutancia > 4000 && e.condutancia <= 4200){
-            status = "yellow";
-			p_atention.innerHTML = `Atenção ${e.condutancia} id ${e.id}`; 
-			p_atention.style.backgroundColor = "black"; 
-			p_atention.style.color = "yellow";
-		}else{
+			p_critical.innerHTML = `${desvio}%  id ${e.id}`;
+			p_critical.style.color = "#871409";
+
+		}else {
 			status = "green";
-		}        
-
-       if(e.id < 2 ){
-         sign = "gray"
-	   }else{
-		 sign = "green"
-	   }        
+		}
 
 
-	 tbody_battery.innerHTML += 
-	 `	
-	  <tbody>  
-
-	      <td data-title="ID"          style="width: auto;">${e.id}</td>
-
-	      <td data-title="Tensao"      style="width: auto;">${e.tensao}</td>
-
-	      <td data-title="Condutancia" style="width: auto;">${e.condutancia}</td>
-
-	      <td data-title="Desvio"      style="width: auto;">${e.desvio}</td>
-
-	      <td data-title="Obs"         style="width: auto;">${e.obs}</td>	
-
-		  <td data-title="Status">
-		     <i class='fa fa-circle' style="color: ${status};"></i>
-		  </td>	
-		  
-          <td data-title="Sign">
-		     <i class='fa fa-signal' style="color: ${sign};"></i>
-		  </td>		  
-
-	  </tbody>	
-	 `
-  })
-
-}
-*/
+		label.push(e.id);
+		//dataBattery.push((e.condutancia / 5000 * 100).toFixed(0));
+		dataBattery.push(desvio);
 
 
-
-
-const displayListBattery = list_battery =>{
-
-   title_list.innerHTML ="List Battery";
-
-   tbody_battery.innerHTML ="";
-
-   list_battery.forEach(e => {
-
-	label.push(e.id);
-	dataBattery.push((e.condutancia / 5000 * 100).toFixed(0));
-
-    let status = "";
-	let sign = "";       
-
-		if (e.condutancia <= 4000) {           
+       /*
+		if (e.condutancia <= 4000) {
 			status = "red";
-			p_critical.innerHTML = `Critico ${e.condutancia} id ${e.id}`; 
-			p_critical.style.backgroundColor = "#757f81"; 
-			p_critical.style.color = "red";
-		}else if(e.condutancia > 4000 && e.condutancia <= 4200){
-            status = "yellow";
-			p_atention.innerHTML = `Atenção ${e.condutancia} id ${e.id}`; 
-			p_atention.style.backgroundColor = "#757f81"; 
+			p_critical.innerHTML = `${(e.condutancia / 5000 * 100).toFixed(0)}%  id ${e.id}`;
+			//p_critical.style.backgroundColor = "#748181"; 
+			p_critical.style.color = "#871409";
+			//p_critical.style.width = "40%";			
+			//p_critical.style.width = "auto";		
+
+		} else if (e.condutancia > 4000 && e.condutancia <= 4200) {
+			status = "yellow";
+			p_atention.innerHTML = `${(e.condutancia / 5000 * 100).toFixed(0)}%  id ${e.id}`;
+			//p_atention.style.backgroundColor = "#748181"; 
 			p_atention.style.color = "yellow";
-		}else{
+			//p_atention.style.width = "40%";
+			//p_atention.style.width = "auto";
+		} else {
 			status = "green";
-		}        
-
-       if(e.id < 2 ){
-         sign = "gray"
-	   }else{
-		 sign = "green"
-	   }  
+		}
+        */
 
 
-	 tbody_battery.innerHTML += 
-	 `	
+		if (e.id < 2) {
+			sign = "gray"
+		} else {
+			sign = "green"
+		}
+
+
+	  tbody_battery.innerHTML +=
+			`	
 	  <tbody>       
-	      <td data-title="ID"          style="width: auto;">${e.id}</td>
-	      <td data-title="Tensao"      style="width: auto;">${e.tensao}</td>
-	      <td data-title="Condutancia" style="width: auto;">${e.condutancia}</td>
-	      <td data-title="Desvio"      style="width: auto;">${e.desvio}</td>
-	      <td data-title="Obs"         style="width: auto;">${e.obs}</td>	
+	      <td data-title="ID">${e.id}</td>
+	      <td data-title="Tensao">${e.tensao}</td>
+	      <td data-title="Condutancia">${e.condutancia}</td>
+	      <td data-title="Desvio">${desvio}%</td>
+	      <td data-title="Obs">${e.obs}</td>	
 		  
-		   <td data-title="Status">
-		     <i class='fa fa-circle' style="color: ${status};"></i>
+		  <td data-title="Status">
+		     <i class='fa fa-circle' style="color: ${status}; font-size:30px;"></i>
 		  </td>	
 		  
           <td data-title="Sign">
-		     <i class='fa fa-signal' style="color: ${sign};"></i>
+		     <i class='fa fa-signal' style="color: ${sign}; font-size:30px;"></i>
 		  </td>
 	  </tbody>	
 	 `
-  })
+	})
 
 }
+
+
 
 
 
@@ -261,38 +262,36 @@ const tbody_report = document.getElementById("tbody-report");
 
 
 
-input_search_analysis.addEventListener("keyup",(e)=>{
-	const search = list_report.filter(i=> i.date.toLowerCase().includes(e.target.value.toLowerCase().replace(/[^0-9]/g, '')));
+input_search_analysis.addEventListener("keyup", (e) => {
+	const search = list_report.filter(i => i.date.toLowerCase().includes(e.target.value.toLowerCase().replace(/[^0-9]/g, '')));
 	displayData(search);
 })
 
 //window.addEventListener("load", displayData.bind(null, list_report));
 
-radio_last.addEventListener("click",()=>{	
-	const search = list_report.slice(-1);    
-    displayData(search);
+radio_last.addEventListener("click", () => {
+	const search = list_report.slice(-1);
+	displayData(search);
 })
 
 
-
-radio_all_report.addEventListener("click",()=>{	
+radio_all_report.addEventListener("click", () => {
 	displayData(list_report);
 })
 
 
 
 
+const displayData = list_report => {
 
-const displayData = list_report =>{
+	title_report.innerHTML = "List Analysis";
 
-   title_report.innerHTML ="List Analysis";
+	tbody_report.innerHTML = "";
 
-   tbody_report.innerHTML ="";
+	list_report.forEach(e => {
 
-   list_report.forEach(e => {
-
-	 tbody_report.innerHTML += 
-	 `	
+		tbody_report.innerHTML +=
+			`	
 	  <tbody>       
 	      <td data-title="ID"          style="width: auto;">${e.id}</td>
 	      <td data-title="Tensao"      style="width: auto;">${e.tensao}</td>
@@ -304,7 +303,7 @@ const displayData = list_report =>{
 	      <td data-title="FK"          style="width: auto;">${e.fk}</td>       
 	  </tbody>	
 	 `
-  })
+	})
 
 }
 
@@ -350,7 +349,6 @@ btn_pdf_report.addEventListener('click', function () {
 
 
 
-
 /*
 btn_surch_ref.onclick = function () {
 	Array.from(document.getElementById('table-report').tBodies).forEach((x, i) => {
@@ -370,7 +368,6 @@ btn_surch_last.onclick = function () {
 	listLastReport();
 };
 */
-
 
 
 
@@ -415,13 +412,97 @@ const listBattery = async () => {
 
 				//onLoadListBattery();
 				//createTableList();
-                displayListBattery(list_battery);
-				
+				displayListBattery(list_battery);
 
 			}
 		)
 
 };
+
+
+
+
+
+
+ 
+
+        
+
+ function lineGraph() {
+
+   const ctx = document.getElementById('myChart');
+
+   var graph = Chart.getChart("graph");
+
+	if (graph) {
+		graph.detroy();
+	}
+
+   graph = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: label,
+			datasets: [{
+				label: '% desvio',
+				data: dataBattery,
+
+				borderWidth: 2,
+				borderColor: '#579f79',
+				backgroundColor:'transparent',
+				
+
+			}]
+		},
+		options: {  
+
+			scales: {
+
+				y: {
+					beginAtZero: true,
+
+			    	grid: {
+                      color: 'rgba(0, 0, 0, 0.3)',
+                    },
+
+                    ticks: {
+                      color: 'black' ,
+					    font: {
+                         size: 12,  
+                         weight: 'bold' 
+                        },
+                    }
+
+				},
+
+
+				x: {
+					beginAtZero: true,
+
+			    	grid: {
+                      color: 'rgba(0, 0, 0, 0.1)'					  
+                    },
+
+                    ticks: {
+                        color: 'black' ,
+					    font: {
+                         size: 12,   
+                         weight: 'bold' 
+                        },
+                    }
+
+				}
+
+
+			},
+		},
+
+	});
+	
+ }
+
+
+
+
 
 
 
@@ -514,7 +595,7 @@ const listReportByFk = async () => {
 					)
 
 					//createTableReport();
-					
+
 
 				} else {
 
@@ -530,13 +611,15 @@ const listReportByFk = async () => {
 			}
 		)
 
-		displayData(list_report);
+	displayData(list_report);
 
 	//console.log("return " + list_report[0].fk);
 
 	return list_report[0].fk;
 
 };
+
+
 
 
 
@@ -611,7 +694,7 @@ const createTableList = () => {
 			p_atention.style.width = "auto";
 
 			content_alert.appendChild(p_atention);
-            content_alert.style.display ="block";
+			content_alert.style.display ="block";
 			content_alert.style.width = "30%";
 			content_alert.style.padding = "10px";
 			content_alert.style.backgroundColor = "#ffffff";
@@ -876,7 +959,70 @@ const createTableReport = () => {
 
 
 
+/*
+const onLoadListBattery = () =>{
 
+   title_list.innerHTML ="List Battery";
+
+   tbody_battery.innerHTML ="";
+
+   list_battery.forEach(e => {
+
+		label.push(e.id);
+		dataBattery.push((e.condutancia / 5000 * 100).toFixed(0));
+
+		let status = "";
+		let sign = "";       
+
+		if (e.condutancia <= 4000) {           
+			status = "red";
+			p_critical.innerHTML = `Critico ${e.condutancia} id ${e.id}`; 
+			p_critical.style.backgroundColor = "black"; 
+			p_critical.style.color = "red";
+		}else if(e.condutancia > 4000 && e.condutancia <= 4200){
+			status = "yellow";
+			p_atention.innerHTML = `Atenção ${e.condutancia} id ${e.id}`; 
+			p_atention.style.backgroundColor = "black"; 
+			p_atention.style.color = "yellow";
+		}else{
+			status = "green";
+		}        
+
+	   if(e.id < 2 ){
+		 sign = "gray"
+	   }else{
+		 sign = "green"
+	   }        
+
+
+	 tbody_battery.innerHTML += 
+	 `	
+	  <tbody>  
+
+		  <td data-title="ID"          style="width: auto;">${e.id}</td>
+
+		  <td data-title="Tensao"      style="width: auto;">${e.tensao}</td>
+
+		  <td data-title="Condutancia" style="width: auto;">${e.condutancia}</td>
+
+		  <td data-title="Desvio"      style="width: auto;">${e.desvio}</td>
+
+		  <td data-title="Obs"         style="width: auto;">${e.obs}</td>	
+
+		  <td data-title="Status">
+			 <i class='fa fa-circle' style="color: ${status};"></i>
+		  </td>	
+		  
+		  <td data-title="Sign">
+			 <i class='fa fa-signal' style="color: ${sign};"></i>
+		  </td>		  
+
+	  </tbody>	
+	 `
+  })
+
+}
+*/
 
 
 
@@ -1039,4 +1185,3 @@ window.addEventListener("load", displayData.bind(null,dados));
 
 }
 */
- 
